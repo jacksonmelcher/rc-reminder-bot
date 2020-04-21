e87b9122c90539a90b498fea153e9b342212aac4
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-04-21 11:49:23 -0700

    A reminder is created when the bot is called and pushed onto a global List. Still a lot of work but this is a good milestone

--- src/express.js
+++ src/express.js
@@ -1,14 +1,22 @@
 "use strict";
 import { put } from "axios";
 import createApp from "ringcentral-chatbot/dist/apps";
+import Reminder from "../models/Reminder";
+import moment from "moment";
+import { v4 as uuidv4 } from "uuid";
+
+let allReminders = [];
+
+let arrayBool = false;
 
 const handle = async (event) => {
-  const { type, text, group, bot, message, data } = event;
+  const { type, text, group, bot, message } = event;
   let args = [];
   let mentionId = "";
   let resMessageString = "";
   let resMessageArray = [];
   let creator = "";
+  let fs = require("fs");
 
   // Spitting and formatting message from user
   if (typeof text !== "undefined") {
@@ -25,15 +33,14 @@ const handle = async (event) => {
   if (typeof message.mentions !== "undefined") {
     mentionId = message.mentions[0].id;
   }
-
+  // Get creators name
   if (typeof bot !== "undefined") {
+    // console.log("GET USER ERROR: " + err=r);
     try {
       const user = await bot.getUser("228768004");
-
       const { name } = user.rc;
-
       creator = name;
-      // console.log("NAME: " + name);
+      reminder.creator = name;
     } catch (error) {
       creator = "Unknown";
       console.log("GET USER ERROR: " + error);
@@ -41,6 +48,27 @@ const handle = async (event) => {
   }
   // For when bot is mentioned in a group.
   if (type === "Message4Bot" && mentionId === "680681005") {
+    arrayBool = true;
+
+    let reminder = new Reminder();
+    reminder.timeCreated = moment();
+    reminder.id = uuidv4();
+    reminder.notificationTime = moment().add(30, "s");
+    reminder.desiredTime = "2 minutes from now";
+    reminder.reminderText = resMessageString;
+    reminder.creator = creator;
+    if (arrayBool === true) {
+      allReminders.push(reminder);
+      allReminders.sort((a, b) => a.notificationTime - b.notificationTime);
+
+      let jsonData = JSON.stringify(allReminders, null, 2);
+      fs.writeFile("json/reminders.json", jsonData, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+    arrayBool = false;
     await bot.sendMessage(group.id, {
       text: `You have a reminder:\n **${resMessageString}** that was made by ${creator}`,
     });
@@ -65,9 +93,28 @@ const handle = async (event) => {
   }
   args = [];
   mentionId = "";
+
+  // for (let i = 0; i < allReminders.length; i++) {
+  //   console.log(
+  //     "====================== ALL REMINDERS =============== \n" +
+  //       allReminders[i].reminderText
+  //   );
+  // }
 };
+
 const app = createApp(handle);
+
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT);
+
+// setInterval(() => {
+//   console.log("================ ALL ===================");
+//   for (let i = 0; i < allReminders.length; i++) {
+//     console.log(
+//       allReminders[i].notificationTime.format("MMMM Do YYYY, h:mm:ss a")
+//     );
+//     console.log(allReminders[i].creator);
+//   }
+// }, 2000);
 
 setInterval(
   async () =>
