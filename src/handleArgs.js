85bd918ae290636f28380de9b69ae7215dfaf8d5
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-01 10:41:12 -0700

    Work in progress

--- src/handleArgs.js
+++ src/handleArgs.js
@@ -1,13 +1,83 @@
 const handleArgs = async (event) => {
+  //──── Responses ─────────────────────────────────────────────────────────────────────────
+  const helpText = {
+    attachments: [
+      {
+        type: 'Card',
+        title: 'Help 🆘',
+        text: 'Here is a list of available commands and an example:',
+        fields: [
+          {
+            title: 'Create a reminder',
+            value:
+              '@Remind **-t** MM/DD/YYYY hh:mm am/pm **-m** Your reminder message',
+            style: 'Long',
+          },
+          {
+            title: 'For help',
+            value: '@Remind **help**',
+            style: 'Short',
+          },
+          {
+            title: 'To submit a bug/issue',
+            value: '@Remind **issue**',
+            style: 'Short',
+          },
+        ],
+        footnote: {
+          text: 'Created and maintained by RC on RC',
+        },
+      },
+    ],
+  };
+
+  const issueText = {
+    attachments: [
+      {
+        type: 'Card',
+        title: 'Issue 🚧',
+        text:
+          'To report a bug please DM Jackson Melcher. Alternatively, if you have a Github you can open an ' +
+          'issue **[here](https://github.com/jacksonmelcher/Glip-Announcements/issues)**',
+        footnote: {
+          text: 'Created and maintained by RC on RC',
+        },
+      },
+    ],
+  };
+
   console.log('----------------In handleArgs--------------------');
   const { text, bot, type, group } = event;
-  console.log(type);
+  if (typeof text !== 'undefined') {
+    console.log(text);
+  }
+
   switch (type) {
+    //========================================================================================
+    /*                                                                                      *
+     *                                      MESSAGE4BOT                                     *
+     *                                                                                      */
+    //========================================================================================
     case 'Message4Bot':
-      await bot.sendMessage(group.id, {
-        text: 'Message for bot',
-      });
+      //──── HELP ──────────────────────────────────────────────────────────────────────────────
+      if (text === '-h' || text === 'help' || text === '-help') {
+        console.log('USER CALLED HELP');
+        await bot.sendMessage(group.id, helpText);
+        return helpText;
+      }
+      //──── ISSUE ─────────────────────────────────────────────────────────────────────────────
+      if (text === '-i' || text === '-issue' || text === 'issue') {
+        console.log('USER CALLED ISSUE');
+        await bot.sendMessage(group.id, issueText);
+        return issueText;
+      }
       break;
+
+    //========================================================================================
+    /*                                                                                      *
+     *                                      BOTJOINGOUP                                     *
+     *                                                                                      */
+    //========================================================================================
     case 'BotJoinGroup':
       await bot.sendMessage(group.id, {
         text: 'group joined',
