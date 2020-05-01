252df81fd60a2f960c53d41952a71df1a26d1677
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-01 10:40:30 -0700

    Added seperate files for the responses to reduce clutter in the main files

---
+++ responses/help.js
@@ -0,0 +1,32 @@
+const helpText = {
+  attachments: [
+    {
+      type: 'Card',
+      title: 'Help 🆘',
+      text: 'Here is a list of available commands and an example:',
+      fields: [
+        {
+          title: 'Create a reminder',
+          value:
+            '@Remind **-t** MM/DD/YYYY hh:mm am/pm **-m** Your reminder message',
+          style: 'Long',
+        },
+        {
+          title: 'For help',
+          value: '@Remind **help**',
+          style: 'Short',
+        },
+        {
+          title: 'To submit a bug/issue',
+          value: '@Remind **issue**',
+          style: 'Short',
+        },
+      ],
+      footnote: {
+        text: 'Created and maintained by RC on RC',
+      },
+    },
+  ],
+};
+
+export default helpText;
---
+++ responses/issue.js
@@ -0,0 +1,16 @@
+const issueText = {
+  attachments: [
+    {
+      type: 'Card',
+      title: 'Issue 🚧',
+      text:
+        'To report a bug please DM Jackson Melcher. Alternatively, if you have a Github you can open an ' +
+        'issue **[here](https://github.com/jacksonmelcher/Glip-Announcements/issues)**',
+      footnote: {
+        text: 'Created and maintained by RC on RC',
+      },
+    },
+  ],
+};
+
+export default issueText;
