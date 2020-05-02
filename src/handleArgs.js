6e28b135e139dab7ef83a3c472eada663b7d88a2
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-01 19:04:41 -0700

    Erased print statements

--- src/handleArgs.js
+++ src/handleArgs.js
@@ -7,12 +7,8 @@ const cyan = "\x1b[36m%s\x1b[0m";
 
 const handleArgs = async (event, print, test) => {
     const { text, bot, type, group } = event;
-    let resMessageArray = [];
-    let resTimeArray = [];
-    let resTimeString = "";
-    let resMessageString = "";
-
     let args = [];
+
     if (typeof event !== "undefined" && print === true) {
         console.log("----------------In handleArgs--------------------");
         // console.log(event);
@@ -28,25 +24,16 @@ const handleArgs = async (event, print, test) => {
         //========================================================================================
         case "Message4Bot":
             if (text === "-h" || text === "help" || text === "-help") {
-                if (print === true) {
-                    console.log(red, "USER CALLED HELP");
-                }
                 if (test !== true) {
                     await bot.sendMessage(group.id, helpText);
                 }
                 return helpText;
             } else if (text === "-i" || text === "-issue" || text === "issue") {
-                if (print === true) {
-                    console.log(red, "USER CALLED ISSUE");
-                }
                 if (test !== true) {
                     await bot.sendMessage(group.id, issueText);
                 }
                 return issueText;
             } else if (args.indexOf("-t") === -1 || args.indexOf("-m") === -1) {
-                if (print === true) {
-                    console.log(red, "NO COMMANDS FOUND");
-                }
                 if (test !== true) {
                     await bot.sendMessage(group.id, noArgsText);
                 }
@@ -69,6 +56,7 @@ const handleArgs = async (event, print, test) => {
             });
             break;
     }
+    return reminderArray;
 };
 
 export default handleArgs;
