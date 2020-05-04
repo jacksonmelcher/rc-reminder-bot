0fed9848d63f9d04a8cb45a568262df4deadeec8
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-04 16:36:18 -0700

    Deleted a tom of stuff, looks much better

--- src/express.js
+++ src/express.js
@@ -5,182 +5,33 @@ import moment from "moment";
 import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 import handleArgs from "./handleArgs";
+import reminderJson from "../json/reminders.json";
 
 let allReminders = [];
 let newReminders = [];
 
 let arrayBool = false;
+// allReminders = JSON.parse(reminderJson);
 
 const handle = async (event) => {
     const { type, text, group, bot, message } = event;
-    let args = [];
-    let mentionId = "";
+
     let resMessageString = "";
-    let resMessageArray = [];
-    let resTimeArray = [];
-    let resTimeString = "";
+
     let fullUserName = "";
 
     const yellow = "\x1b[33m%s\x1b[0m";
     const cyan = "\x1b[36m%s\x1b[0m";
-    // let dummyText = `-m do 25 pushups -t ${moment()
-    //   .add(1, 'minute')
-    //   .format('MM/DD/YYY hh:mm a')}`;
 
     let newReminders = await handleArgs(event, true, false);
     newReminders.sort((a, b) => a.reminderTime - b.reminderTime);
-    console.log("Length: " + newReminders.length);
+    // allReminders = allReminders.concat(newReminders);
+    // allReminders.sort((a, b) => a.reminderTime - b.reminderTime);
+    // console.log("Length: " + newReminders.length);
 
-    // console.log(
-    //     "==================EVENT==================" +
-    //         JSON.stringify(event, null, 2)
-    // );
-
-    // ANCHOR Direct message handling. Does not support mentions to other teams
-    // if (type === "Message4Bot") {
-    // ANCHOR Command line args handling
-    // if (typeof text !== "undefined") {
-    // args = dummyText.split(' ');
-    // args = text.split(" ");
-    // console.log("ARGS: ", args);
-
-    // if (
-    //   args.toString() === 'help' ||
-    //   args.toString() === '-h' ||
-    //   args.toString() === '-help'
-    // ) {
-    //   console.log('User asked for help');
-    //   await bot.sendMessage(group.id, {
-    //     attachments: [
-    //       {
-    //         type: 'Card',
-    //         title: 'Help 🆘',
-    //         text: 'Here is a list of available commands and an example:',
-    //         fields: [
-    //           {
-    //             title: 'Create a reminder',
-    //             value:
-    //               '@Remind **-t** MM/DD/YYYY hh:mm am/pm **-m** Your reminder message',
-    //             style: 'Long',
-    //           },
-    //           {
-    //             title: 'For help',
-    //             value: '@Remind **help**',
-    //             style: 'Short',
-    //           },
-    //           {
-    //             title: 'To submit a bug/issue',
-    //             value: '@Remind **issue**',
-    //             style: 'Short',
-    //           },
-    //         ],
-    //         footnote: {
-    //           text: 'Created and maintained by RC on RC',
-    //         },
-    //       },
-    //     ],
-    //   });
-    // } else
-    // if (
-    //   args.toString() === 'issue' ||
-    //   args.toString() === '-i' ||
-    //   args.toString() === '-issue' ||
-    //   args.toString() === '-I'
-    // ) {
-    //   console.log('User has an issue');
-    //   await bot.sendMessage(group.id, {
-    //     // text:
-    //     //   'To report a bug please DM Jackson Melcher. Alternatively, if you have a Github you can open an ' +
-    //     //   'issue **[here](https://github.com/jacksonmelcher/Glip-Announcements/issues)**',
-    //     attachments: [
-    //       {
-    //         type: 'Card',
-    //         title: 'Issue 🚧',
-    //         text:
-    //           'To report a bug please DM Jackson Melcher. Alternatively, if you have a Github you can open an ' +
-    //           'issue **[here](https://github.com/jacksonmelcher/Glip-Announcements/issues)**',
-    //         footnote: {
-    //           text: 'Created and maintained by RC on RC',
-    //         },
-    //       },
-    //     ],
-    //   });
-    // } else
-    // if (args.indexOf('-t') === -1 || args.indexOf('-m') === -1) {
-    //   //──── No text added ─────────────────────────────────────────────────────────────────────
-    //   console.log('No keywords found');
-
-    //   await bot.sendMessage(group.id, {
-    //     attachments: [
-    //       {
-    //         type: 'Card',
-    //         title: '🚨 You forgot to add a **-m** or **-t** 🚨',
-    //         text:
-    //           'For help type **@Reminder help** for an example and a list of commands.',
-    //         footnote: {
-    //           text: 'Created and maintained by RC on RC',
-    //         },
-    //       },
-    //     ],
-    //   });
-    // }
-    // else {
-    // Check to see which argument is first
-    //   if (args.indexOf('-t') > args.indexOf('-m')) {
-    //     console.log('Message came first.');
-    //     for (let i = args.indexOf('-m') + 1; i < args.indexOf('-t'); i++) {
-    //       resMessageArray.push(args[i]);
-    //     }
-
-    //     for (let i = args.indexOf('-t') + 1; i < args.length; i++) {
-    //       resTimeArray.push(args[i]);
-    //     }
-    //     // FIXME hardcoded time
-    //     // resTimeString = moment().add(1, 'minute').format('MM/DD/YYY hh:mm');
-    //     resTimeString = resTimeArray.toString().replace(/,/g, ' ');
-    //     resMessageString = resMessageArray.toString().replace(/,/g, ' ');
-    //     console.log(resMessageString + ' -- ' + resTimeString);
-    //   } else {
-    //     console.log('Time came first.');
-    //     for (let i = args.indexOf('-t') + 1; i < args.indexOf('-m'); i++) {
-    //       resMessageArray.push(args[i]);
-    //     }
-
-    //     for (let i = args.indexOf('-m') + 1; i < args.length; i++) {
-    //       resTimeArray.push(args[i]);
-    //     }
-
-    //     resMessageString = resTimeArray.toString().replace(/,/g, ' ');
-    //     resTimeString = resMessageArray.toString().replace(/,/g, ' ');
-    //     console.log(resMessageString + ' -- ' + resTimeString);
-    //   }
-    //   // }
-    // }
-    // if (typeof message !== 'undefined') {
-    // if (typeof message.mentions !== "undefined") {
-    //     try {
-    //         mentionId = message.mentions[0].id;
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-    // }
-    // Get creators name
-    // if (typeof bot !== "undefined" && event !== "undefined") {
-    //     try {
-    //         const user = await bot.getUser(event.userId);
-    //         const { name } = user.rc;
-    //         fullUserName = name;
-    //     } catch (error) {
-    //         console.log("GET USER ERROR: " + error + " name: " + fullUserName);
-    //     }
-    // }
-
-    // if (mentionId === '680681005' && text)
-    // if (mentionId === "680681005") {
     arrayBool = true;
 
-    let reminder = new Reminder();
+    // let reminder = new Reminder();
     // Check to ee if reminder is in the past
     console.log(yellow, "In express");
 
@@ -205,20 +56,6 @@ const handle = async (event) => {
             console.log("}");
         }
     }
-    // if(resTimeString === 'Invalid Date' |||)
-    // if (moment() < moment(resTimeString, "MM/DD/YY hh:mm a")) {
-    // reminder.timeCreated = moment();
-    // reminder.id = uuidv4();
-    // reminder.notificationTime = moment(resTimeString, "MM/DD/YY hh:mm a");
-    // reminder.reminderText = resMessageString;
-    // reminder.creator = fullUserName;
-    // reminder.duration = moment
-    //     .duration(reminder.notificationTime.diff(reminder.timeCreated))
-    //     .as("milliseconds");
-
-    // let duration = moment.duration(
-    //     reminder.notificationTime.diff(reminder.timeCreated)
-    // );
 
     if (arrayBool === true) {
         let jsonData = JSON.stringify(newReminders, null, 2);
@@ -229,24 +66,6 @@ const handle = async (event) => {
         });
     }
     arrayBool = false;
-
-    // await bot.sendMessage(group.id, {
-    //     text: `Reminder set ⏰, I will send you a reminder in **${duration.humanize()}**`,
-    // });
-    // }
-    // else
-    // if (moment() >= moment(resTimeString, "MM/DD/YY hh:mm a")) {
-    //     await bot.sendMessage(group.id, {
-    //         text: `The time you gave me already happened`,
-    //     });
-    // }
-    // FIXME
-    // else {
-    //   await bot.sendMessage(group.id, {
-    //     text: `🚨Please send a the time in a valid format. **MM/DD/YY hh:mm am/pm**🚨`,
-    //   });
-    // }
-    // }
 
     // ANCHOR Set timeout
     if (allReminders.length > 0) {
@@ -265,52 +84,6 @@ const handle = async (event) => {
             });
         }, allReminders[0].duration);
     }
-
-    // ANCHOR group Joined
-    // console.log("=========================TYPE=================\n" + type);
-    // if (type === "BotJoinGroup") {
-    //     console.log("GROUP JOINED: " + JSON.stringify(group, null, 2));
-    //     await bot.sendMessage(group.id, {
-    //         attachments: [
-    //             {
-    //                 type: "Card",
-
-    //                 author: {
-    //                     name: "Reminder Bot",
-    //                     // uri: 'https://example.com/author_link',
-    //                     // iconUri: 'https://example.com/author_icon.png',
-    //                 },
-    //                 title: "Instructions",
-    //                 text:
-    //                     "Hi, I am a reminder bot. I can be used to remind you or a whole team of items at a specified " +
-    //                     "time. To use me you can send me a direct message or add me to a team. I am the first iteration" +
-    //                     " and lack features. As time passes and with your feedback, I will be updated with new features.",
-
-    //                 fields: [
-    //                     {
-    //                         title: "Create a reminder",
-    //                         value:
-    //                             "@Remind **-t** MM/DD/YYYY hh:mm am/pm **-m** Your reminder message",
-    //                         style: "Long",
-    //                     },
-    //                     {
-    //                         title: "For help",
-    //                         value: "@Remind **help**",
-    //                         style: "Short",
-    //                     },
-    //                     {
-    //                         title: "To submit a bug/issue",
-    //                         value: "@Remind **issue**",
-    //                         style: "Short",
-    //                     },
-    //                 ],
-    //                 footnote: {
-    //                     text: "Created and maintained by RC on RC",
-    //                 },
-    //             },
-    //         ],
-    //     });
-    // }
 
     args = [];
     mentionId = "";
