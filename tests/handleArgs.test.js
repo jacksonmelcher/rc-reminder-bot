f67a2393618e203529ddabfd15e0cbcba4c4c4a3
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-04 11:00:18 -0700

    Added test for bot joined group

--- tests/handleArgs.test.js
+++ tests/handleArgs.test.js
@@ -1,33 +1,55 @@
-import handleArgs from '../src/handleArgs';
-import { helpText, issueText, noArgsText } from '../responses/index';
+import handleArgs from "../src/handleArgs";
+import {
+    helpText,
+    issueText,
+    noArgsText,
+    joinedGroup,
+} from "../responses/index";
 
 const testHelpEvent = {
-    type: 'Message4Bot',
-    text: 'help',
+    type: "Message4Bot",
+    text: "help",
 };
 const testIssueEvent = {
-    type: 'Message4Bot',
-    text: 'issue',
+    type: "Message4Bot",
+    text: "issue",
 };
 const testNoArgsEvent = {
-    type: 'Message4Bot',
-    text: '',
+    type: "Message4Bot",
+    text: "",
 };
 
-test('should output help text', async () => {
+const testJoinedEvent = {
+    type: "BotJoinGroup",
+    text: "",
+};
+
+it("should output help text", async () => {
     const returned = await handleArgs(testHelpEvent, false, true);
 
     expect(returned).toBe(helpText);
 });
 
-test('should output issue text', async () => {
+it("should output issue text", async () => {
     const returned = await handleArgs(testIssueEvent, false, true);
 
     expect(returned).toBe(issueText);
 });
 
-test('should output noArgs text', async () => {
+it("should output noArgs text", async () => {
     const returned = await handleArgs(testNoArgsEvent, false, true);
 
     expect(returned).toBe(noArgsText);
+});
+
+it("should output noArgs text", async () => {
+    const returned = await handleArgs(testNoArgsEvent, false, true);
+
+    expect(returned).toBe(noArgsText);
+});
+
+test("should output bot JoinedGroup text", async () => {
+    const returned = await handleArgs(testJoinedEvent, false, true);
+
+    expect(returned).toBe(joinedGroup);
 });
