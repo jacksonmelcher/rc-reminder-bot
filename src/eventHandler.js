5f90b8cce10bb38fbc691cbb5fe17b9b27884ab6
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-07 13:20:37 -0700

    Renamed file. Found out there has been a built in bot database the whole time

---
+++ src/eventHandler.js
@@ -0,0 +1,98 @@
+import {
+    issueText,
+    helpText,
+    noArgsText,
+    joinedGroup,
+    timeAlreadyHappened,
+} from "../responses/index";
+import { createReminder } from "./createReminder";
+import { Service, Bot } from "ringcentral-chatbot/dist/models";
+import handle from "./express";
+
+let reminderArray = [];
+const red = "\x1b[42m%s\x1b[0m";
+const cyan = "\x1b[36m%s\x1b[0m";
+
+export const eventHandler = async (event) => {
+    const { text, bot, type, group } = event;
+
+    switch (type) {
+        case "Message4Bot":
+            await handleMessage4Bot(event);
+
+            break;
+
+        case "BotJoinGroup":
+    }
+    // // console.log(reminderArray);
+    return reminderArray;
+};
+
+const handleMessage4Bot = async (event) => {
+    const { text, group, bot } = event;
+    let args = [];
+
+    if (typeof text !== "undefined") {
+        args = text.split(" ");
+    }
+    if (text === "-h" || text === "help" || text === "-help") {
+        console.log("USER ASKED FOR HELP");
+
+        await bot.sendMessage(group.id, helpText);
+
+        return helpText;
+    } else if (text === "-i" || text === "-issue" || text === "issue") {
+        console.log("USER HAS ISSUE");
+
+        await bot.sendMessage(group.id, issueText);
+
+        return issueText;
+    } else if (args.indexOf("-t") === -1 || args.indexOf("-m") === -1) {
+        console.log("NO -t OR -m");
+
+        await bot.sendMessage(group.id, noArgsText);
+
+        return noArgsText;
+    } else if (args.includes("-t") && args.includes("-m")) {
+        console.log("THINGS ARE WORKING");
+        const message = await createReminder(args, event);
+        let text = message.text;
+        let timeCreated = message.timeCreated;
+        let creator = message.creator;
+        let reminderTime = message.reminderTime;
+        let duration = message.duration;
+        const service = await Service.create({
+            name: "Remind",
+            botId: bot.id,
+            groupId: group.id,
+            userId: message.creatorId,
+            data: {
+                text: message.text,
+                timeCreated: message.timeCreated,
+                creator: message.creator,
+                reminderTime: message.reminderTime,
+                duration: message.duration,
+            },
+        });
+        console.log(
+            red,
+            `Service: ${service.id} - ${service.data.creator} - ${service.groupId} - ${service.data.text} `
+        );
+        if (message === false) {
+            console.log("TIME ALREADY HAPPENED");
+            // if (test !== true) {
+            //     await bot.sendMessage(group.id, timeAlreadyHappened);
+            // }
+        } else {
+            await bot.sendMessage(group.id, {
+                text: `Reminder set ⏰, I will send you a reminder in **${message.duration.humanize()}**`,
+            });
+
+            reminderArray.push(message);
+        }
+    }
+};
+
+const handleBotJoinedGroup = async ({ group }) => {
+    await bot.sendMessage(group.id, joinedGroup);
+};
