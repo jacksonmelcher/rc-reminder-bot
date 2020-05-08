5838977c2bd0d5d65789029ee1ff15438a597bae
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-08 11:46:22 -0700

    New rinder file

---
+++ src/remind.js
@@ -0,0 +1,13 @@
+import { Service, Bot } from "ringcentral-chatbot/dist/models";
+
+const remind = async () => {
+    const services = await Service.findAll({ where: { name: "Remind" } });
+    console.log(services.length);
+    // for (const service in services) {
+    //     const group = await service.groupId;
+
+    //     console.log(group);
+    // }
+};
+
+export default remind;
