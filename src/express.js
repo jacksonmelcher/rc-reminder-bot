6de3b8907f96a9f66c0e674b5be5add9fdd2ddb5
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-11 15:07:24 -0700

    Deleted unused stuff

--- src/express.js
+++ src/express.js
@@ -1,23 +1,13 @@
 import { put } from "axios";
 import createApp from "ringcentral-chatbot/dist/apps";
-// import moment from "moment";
-// import fs from "fs";
+
 import { eventHandler } from "./eventHandler";
 import remind from "./remind";
-
-// const yellow = "\x1b[33m%s\x1b[0m";
-// const cyan = "\x1b[36m%s\x1b[0m";
-// const red = "\x1b[42m%s\x1b[0m";
-
-let newReminders = [];
-
-let arrayBool = false;
 
 const handle = async (event) => {
     await eventHandler(event);
 };
 
-// ANCHOR Array monitor and manipulation
 setInterval(() => remind(), 2000);
 
 const app = createApp(handle);
