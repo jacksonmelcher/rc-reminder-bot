7d4c4693294da1e2026f8677eac8bb01bcbf6898
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-20 12:43:59 -0700

    Fixing port issue

--- src/express.js
+++ src/express.js
@@ -12,7 +12,7 @@ const handle = async (event) => {
 
 const app = createApp(handle);
 
-app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT);
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
 
 setInterval(
     () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`),
